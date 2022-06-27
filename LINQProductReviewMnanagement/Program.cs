7d75971ec5d8e3255363294f72ce6b0e2414cbb5
[prasanna] <prasanna>
2022-06-27 13:21:04 +0530

    [prasanna]: create product reviews

---
+++ LINQProductReviewMnanagement/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace LINQProductReviewMnanagement
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to product review management problem ststement:");
+            List<ProductReview> ProductReviewList = new List<ProductReview>()
+            {
+                new ProductReview(){ProductID=1, UserID=1, Rating=5,Review="Good",IsLike=true},
+                new ProductReview(){ProductID=2, UserID=1, Rating=5,Review="Good",IsLike=true},
+                new ProductReview(){ProductID=3, UserID=2, Rating=5,Review="Good",IsLike=true},
+                new ProductReview(){ProductID=4, UserID=2, Rating=5,Review="Good",IsLike=true},
+                new ProductReview(){ProductID=5, UserID=1, Rating=4,Review="Good",IsLike=true},
+                new ProductReview(){ProductID=6, UserID=3, Rating=5,Review="Good",IsLike=true},
+                new ProductReview(){ProductID=7, UserID=3, Rating=5,Review="Good",IsLike=true},
+                new ProductReview(){ProductID=8, UserID=3, Rating=5,Review="nice",IsLike=true},
+                new ProductReview(){ProductID=9, UserID=4, Rating=5,Review="Good",IsLike=true},
+                new ProductReview(){ProductID=10, UserID=5, Rating=5,Review="Good",IsLike=true},
+                new ProductReview(){ProductID=12, UserID=5, Rating=5,Review="Good",IsLike=true},
+                new ProductReview(){ProductID=13, UserID=1, Rating=4,Review="Bad",IsLike=true},
+                new ProductReview(){ProductID=14, UserID=1, Rating=5,Review="Good",IsLike=true},
+                new ProductReview(){ProductID=15, UserID=1, Rating=5,Review="Good",IsLike=true},
+                new ProductReview(){ProductID=16, UserID=10, Rating=2,Review="Good",IsLike=true},
+                new ProductReview(){ProductID=17, UserID=10, Rating=5,Review="Good",IsLike=true},
+                new ProductReview(){ProductID=18, UserID=10, Rating=5,Review="Good",IsLike=true},
+                new ProductReview(){ProductID=19, UserID=10, Rating=5,Review="Good",IsLike=true},
+                new ProductReview(){ProductID=20, UserID=10, Rating=5,Review="Good",IsLike=true},
+                new ProductReview(){ProductID=21, UserID=10, Rating=5,Review="nice",IsLike=true},
+                new ProductReview(){ProductID=22, UserID=10, Rating=2,Review="nice",IsLike=true},
+                new ProductReview(){ProductID=23, UserID=10, Rating=5,Review="nice",IsLike=true},
+                new ProductReview(){ProductID=24, UserID=10, Rating=5,Review="nice",IsLike=true},
+                new ProductReview(){ProductID=25, UserID=10, Rating=5,Review="nice",IsLike=true},
+            };
+            LINQProductReviewMnanagement.ReviewManagement review = new LINQProductReviewMnanagement.ReviewManagement();
+            review.getProductReview(ProductReviewList);
+
+        }
+    }
+}
---
+++ LINQProductReviewMnanagement/ReviewManagement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQProductReviewMnanagement
+{
+    public class ReviewManagement
+    {
+        public void getProductReview(List<ProductReview> productReviewsList)
+        {
+            foreach (var list in productReviewsList)
+            {
+                Console.WriteLine("ProductID:-" + list.ProductID + " " + "UserID:-" + list.UserID + " " + "Rating:-" + list.Rating + " " + "Review:-" + list.Review + " " + "IsLike:-" + list.IsLike);
+            }
+
+        }
+    }
+}
