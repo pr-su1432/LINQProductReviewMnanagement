370e472c6eb944284d8e7e7c01f3d263dec50918
[prasanna] <prasanna>
2022-06-27 16:12:15 +0530

    [prasanna]: skip top 5 recors from the list

--- LINQProductReviewMnanagement/Program.cs
+++ LINQProductReviewMnanagement/Program.cs
@@ -10,37 +10,38 @@ namespace LINQProductReviewMnanagement
             Console.WriteLine("welcome to product review management problem ststement:");
             List<ProductReview> ProductReviewList = new List<ProductReview>()
             {
-                new ProductReview(){ProductID=1, UserID=1, Rating=9,Review="Good",IsLike=true},
+                new ProductReview(){ProductID=1, UserID=1, Rating=9,Review="Good",IsLike=false},
                 new ProductReview(){ProductID=2, UserID=1, Rating=5,Review="Good",IsLike=true},
-                new ProductReview(){ProductID=3, UserID=2, Rating=5,Review="Good",IsLike=true},
+                new ProductReview(){ProductID=3, UserID=2, Rating=5,Review="Good",IsLike=false},
                 new ProductReview(){ProductID=4, UserID=2, Rating=5,Review="Good",IsLike=true},
-                new ProductReview(){ProductID=5, UserID=1, Rating=4,Review="Good",IsLike=true},
+                new ProductReview(){ProductID=5, UserID=1, Rating=4,Review="very bad",IsLike=true},
                 new ProductReview(){ProductID=6, UserID=3, Rating=5,Review="Good",IsLike=true},
                 new ProductReview(){ProductID=7, UserID=3, Rating=5,Review="Good",IsLike=true},
-                new ProductReview(){ProductID=8, UserID=3, Rating=5,Review="nice",IsLike=true},
+                new ProductReview(){ProductID=8, UserID=3, Rating=5,Review="nice",IsLike=false},
                 new ProductReview(){ProductID=9, UserID=4, Rating=5,Review="Good",IsLike=true},
                 new ProductReview(){ProductID=10, UserID=5, Rating=5,Review="Good",IsLike=true},
-                new ProductReview(){ProductID=12, UserID=5, Rating=5,Review="Good",IsLike=true},
+                new ProductReview(){ProductID=12, UserID=5, Rating=5,Review="Good",IsLike=false},
                 new ProductReview(){ProductID=13, UserID=1, Rating=4,Review="Bad",IsLike=true},
-                new ProductReview(){ProductID=14, UserID=1, Rating=5,Review="Good",IsLike=true},
+                new ProductReview(){ProductID=14, UserID=1, Rating=5,Review="Good",IsLike=false},
                 new ProductReview(){ProductID=15, UserID=1, Rating=5,Review="Good",IsLike=true},
-                new ProductReview(){ProductID=16, UserID=10, Rating=2,Review="Good",IsLike=true},
+                new ProductReview(){ProductID=16, UserID=10, Rating=2,Review="Good",IsLike=false},
                 new ProductReview(){ProductID=17, UserID=10, Rating=6,Review="Good",IsLike=true},
                 new ProductReview(){ProductID=18, UserID=10, Rating=7,Review="Good",IsLike=true},
-                new ProductReview(){ProductID=19, UserID=10, Rating=8,Review="Good",IsLike=true},
+                new ProductReview(){ProductID=19, UserID=10, Rating=8,Review="not good",IsLike=false},
                 new ProductReview(){ProductID=20, UserID=10, Rating=5,Review="Good",IsLike=true},
                 new ProductReview(){ProductID=21, UserID=10, Rating=5,Review="nice",IsLike=true},
-                new ProductReview(){ProductID=22, UserID=10, Rating=2,Review="nice",IsLike=true},
-                new ProductReview(){ProductID=23, UserID=10, Rating=5,Review="nice",IsLike=true},
-                new ProductReview(){ProductID=24, UserID=10, Rating=5,Review="nice",IsLike=true},
-                new ProductReview(){ProductID=25, UserID=10, Rating=5,Review="nice",IsLike=true},
+                new ProductReview(){ProductID=22, UserID=10, Rating=2,Review="nice",IsLike=false},
+                new ProductReview(){ProductID=23, UserID=10, Rating=5,Review="super",IsLike=true},
+                new ProductReview(){ProductID=24, UserID=10, Rating=5,Review="nice",IsLike=false},
+                new ProductReview(){ProductID=25, UserID=10, Rating=5,Review="nice",IsLike=false},
             };
             LINQProductReviewMnanagement.ReviewManagement review = new LINQProductReviewMnanagement.ReviewManagement();
             //review.getProductReview(ProductReviewList);
             //review.TopRatedRecords(ProductReviewList);
             //review.RecordsOfratingsGreaterThan3(ProductReviewList);
             //review.retriveCountOfRecords(ProductReviewList);
-            review.RetriveproductIDAndReview(ProductReviewList);
+            //review.RetriveproductIDAndReview(ProductReviewList);
+            review.recorsSkipTop5Records(ProductReviewList);
 
         }
     }
--- LINQProductReviewMnanagement/ReviewManagement.cs
+++ LINQProductReviewMnanagement/ReviewManagement.cs
@@ -22,8 +22,8 @@ namespace LINQProductReviewMnanagement
         public void TopRatedRecords(List<ProductReview> productReviewList)
         {
             var data = (from ProductReview in productReviewList
-                          orderby ProductReview.Rating descending
-                          select ProductReview).Take(3);
+                        orderby ProductReview.Rating descending
+                        select ProductReview).Take(3);
             foreach (var list in data)
             {
                 Console.WriteLine("ProductID:-" + list.ProductID + " " + "UserID:-" + list.UserID + " " + "Rating:-" + list.Rating + " " + "Review:-" + list.Review + " " + "IsLike:-" + list.IsLike);
@@ -32,9 +32,9 @@ namespace LINQProductReviewMnanagement
         public void RecordsOfratingsGreaterThan3(List<ProductReview> productReviewList)
         {
             var data = (from productReview in productReviewList
-                          where productReview.Rating > 3 &&
-                          (productReview.ProductID == 1 || productReview.ProductID == 4 || productReview.ProductID == 9)
-                          select productReview);
+                        where productReview.Rating > 3 &&
+                        (productReview.ProductID == 1 || productReview.ProductID == 4 || productReview.ProductID == 9)
+                        select productReview);
             Console.WriteLine("Retrive all records of ratings greater than 3 and who's product ID is 1 or 4 or 9.");
             foreach (var list in data)
             {
@@ -53,11 +53,21 @@ namespace LINQProductReviewMnanagement
         public void RetriveproductIDAndReview(List<ProductReview> productReviewList)
         {
             var data = (from ProductReview in productReviewList
-                          select new { ProductID = ProductReview.ProductID, Review = ProductReview.Review });
+                        select new { ProductID = ProductReview.ProductID, Review = ProductReview.Review });
             Console.WriteLine("Product ID|Review");
             foreach (var list in data)
             {
                 Console.WriteLine(list.ProductID + "--->" + list.Review);
+            }
+        }
+        public void recorsSkipTop5Records(List<ProductReview> productReviewList)
+        {
+            var data = (from productReview in productReviewList
+                        select productReview).Skip(5);
+            Console.WriteLine("Records after skiping top 5.");
+            foreach (var list in data)
+            {
+                Console.WriteLine("ProductID:-" + list.ProductID + " " + "UserID:-" + list.UserID + " " + "Rating:-" + list.Rating + " " + "Review:-" + list.Review + " " + "IsLike:-" + list.IsLike);
             }
         }
     }
